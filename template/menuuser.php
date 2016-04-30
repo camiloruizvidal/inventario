c23831055edc0b54e62fddf933a62f6b76a19d5b
camiloruizvidal <camiloruizvidal@gmail.com>
2016-04-30 00:18:17 -0500

    #006

--- template/menuuser.php
+++ template/menuuser.php
@@ -1,8 +1,17 @@
 <?php
 @session_start();
-if(!$_SESSION)
+if (!$_SESSION)
 {
     header('location: login.php');
+}
+$locationmenu = $_SERVER['REQUEST_URI'];
+$locationmenu = (end(explode('/', $locationmenu)));
+$active       = array('', '', '', '', '', '');
+switch ($locationmenu)
+{
+    case 'productos_new.php':$active[6] = ' class="active"';
+        break;
+    default :$active[0] = ' class="active"';
 }
 ?>
 <nav class="navbar navbar-default navbar-fixed-top navbar-inner">
@@ -29,7 +38,7 @@ if(!$_SESSION)
                 </li>
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        <i class="icon-user"></i> <?php echo $_SESSION['username']?> <b class="caret"></b>
+                        <i class="icon-user"></i> <?php echo $_SESSION['username'] ?> <b class="caret"></b>
                     </a>
                     <ul class="dropdown-menu">
                         <li><a href="javascript:;">Perfil</a></li>
@@ -44,13 +53,13 @@ if(!$_SESSION)
     <div class="subnavbar-inner">
         <div class="container">
             <ul class="mainnav">
-                <li class="active"><a href="index.php"><i class="icon-dashboard"></i><span>Panel</span> </a> </li>
-                <li><a href="reports.php"><i class="icon-list-alt"></i><span>Reportes</span> </a> </li>
-                <li><a href="guidely.php"><i class="icon-facetime-video"></i><span>Tour por la app</span> </a></li>
-                <li><a href="calendar.php"><i class="icon-calendar"></i><span>Calendario</span> </a></li>
-                <li><a href="charts.php"><i class="icon-bar-chart"></i><span>Gráficas</span> </a> </li>
-                <li><a href="shortcodes.php"><i class="icon-code"></i><span>Código fuente</span> </a> </li>
-                <li><a href="productos_new.php"><i class="icon-truck"></i><span>Código fuente</span> </a> </li>
+                <li<?php echo $active[0]; ?>><a href="index.php"><i class="icon-dashboard"></i><span>Panel</span> </a> </li>
+                <li<?php echo $active[1]; ?>><a href="reports.php"><i class="icon-list-alt"></i><span>Reportes</span> </a> </li>
+                <li<?php echo $active[2]; ?>><a href="guidely.php"><i class="icon-facetime-video"></i><span>Tour por la app</span> </a></li>
+                <li<?php echo $active[3]; ?>><a href="calendar.php"><i class="icon-calendar"></i><span>Calendario</span> </a></li>
+                <li<?php echo $active[4]; ?>><a href="charts.php"><i class="icon-bar-chart"></i><span>Gráficas</span> </a> </li>
+                <li<?php echo $active[5]; ?>><a href="shortcodes.php"><i class="icon-code"></i><span>Código fuente</span> </a> </li>
+                <li<?php echo $active[6]; ?>><a href="productos_new.php"><i class="icon-truck"></i><span>Código fuente</span> </a> </li>
             </ul>
         </div>
     </div>
